48f8a398cdcf65808fb783926334b2c2e1269b36
FrankieFabuloso <frocha@ucsc.edu>
2017-05-05 14:47:03 -0700

    Centers contact form. Underlines apporpriate navbar items based on route. Other cleaning of code

--- routes/index.js
+++ routes/index.js
@@ -3,22 +3,18 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  console.log('hello:')
   res.render('projects/index', { title: 'Frankie' });
 });
 
 router.get('/blog', function(req, res, next) {
-  console.log('hello:')
   res.render('projects/index', { title: 'blog' });
 });
 
 router.get('/about', function(req, res, next) {
-  console.log('hello:')
   res.render('about/index', { title: 'about' });
 });
 
 router.get('/contact', function(req, res, next) {
-  console.log('hello:')
   res.render('contact/index', { title: 'contact' });
 });
 
